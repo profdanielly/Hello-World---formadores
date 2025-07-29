4556ba2452f53baeaac35ca7f4b004b8eb92c1a6
profdanielly <souza.santos.danielly@escola.pr.gov.br>
2025-07-29 20:15:22 -0300

    java script

--- script.js
+++ script.js
@@ -7,4 +7,19 @@ inserirNome();
 
 let linguagens = ["Javascript", "Python", "C"];
 const item = document.querySelector("#lista");
+item.textContent = linguagens[2];
+item.textContent = linguagens[1];
 item.textContent = linguagens[0];
+
+let aluno1 = {
+  nome: "Gabriel",
+  idade: 13,
+  anoLetivo: "2ºsérie",
+  materiasFavoritas: ["Geografia","Programação","Arte"]
+}
+
+console.log(aluno1.nome); // “Gabriel”
+console.log(aluno1.idade); // 13
+console.log(aluno1.materiasFavoritas);
+// ["Geografia","Programação","Arte"]
+console.log(aluno1.materiasFavoritas[1]);
